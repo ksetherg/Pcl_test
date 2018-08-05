0fe9431502cf26ed390a8030654534629b91c339
ksetherg <ksetherg@gmail.com>
2018-08-05 15:54:44 +0300

    added some new features

--- my_grand_project.cpp
+++ my_grand_project.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-
 #include <pcl/point_cloud.h>
 #include <pcl/common/io.h>
 #include <pcl/common/copy_point.h>
@@ -18,16 +17,16 @@
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/thread/thread.hpp>
 #include <math.h>
+
 int create_cloud (int, int);
 bool load_cloud (const std::string &, pcl::PointCloud<pcl::PointXYZ> &);
 bool save_cloud (const std::string &, const pcl::PointCloud<pcl::PointXYZ> &);
-void add_gaussian_noise(pcl::PointCloud<pcl::PointXYZ> &, float, float);
+void add_gaussian_noise(pcl::PointCloud<pcl::PointXYZ> &, pcl::PointCloud<pcl::PointXYZ> &, float, float);
 double L2norm(pcl::PointXYZ &, pcl::PointXYZ &);
 double compute_metric_abs(pcl::PointCloud<pcl::PointXYZ> &);
 double compute_metric_mean(pcl::PointCloud<pcl::PointXYZ> &);
 
-const pcl::PointXYZ obs = pcl::PointXYZ(0., 0., 0.);//наблюдатель
-
+const pcl::PointXYZ obs = pcl::PointXYZ(0., 0., 0.); //наблюдатель
 
 int
 create_cloud (int w, int h)
@@ -83,13 +82,14 @@ save_cloud(const std::string &filename, const pcl::PointCloud<pcl::PointXYZ> &ou
 }
 
 void 
-add_gaussian_noise(pcl::PointCloud<pcl::PointXYZ> &input, float mean, float disp)
+add_gaussian_noise( pcl::PointCloud<pcl::PointXYZ> &input,
+                    pcl::PointCloud<pcl::PointXYZ> &noized_cloud,
+                    float mean, float disp)
 {   
     //просто добавляем шумм из нормального распределения с матожиданием mean и дисперсией disp
-    pcl::PointCloud<pcl::PointXYZ>::Ptr noized_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    noized_cloud->points.resize(input.points.size());
-    noized_cloud->width = input.width;
-    noized_cloud->height = input.height;
+    noized_cloud.points.resize(input.points.size());
+    noized_cloud.width = input.width;
+    noized_cloud.height = input.height;
 /* 
     std::default_random_engine generator;
     std::normal_distribution<double> dist(mean, disp);
@@ -108,12 +108,10 @@ add_gaussian_noise(pcl::PointCloud<pcl::PointXYZ> &input, float mean, float disp
 
     for (size_t point_i = 0; point_i < input.points.size (); ++point_i)
     {
-        noized_cloud->points[point_i].x = input.points[point_i].x + static_cast<float> (var_nor ());
-        noized_cloud->points[point_i].y = input.points[point_i].y + static_cast<float> (var_nor ());
-        noized_cloud->points[point_i].z = input.points[point_i].z + static_cast<float> (var_nor ());
+        noized_cloud.points[point_i].x = input.points[point_i].x + static_cast<float> (var_nor ());
+        noized_cloud.points[point_i].y = input.points[point_i].y + static_cast<float> (var_nor ());
+        noized_cloud.points[point_i].z = input.points[point_i].z + static_cast<float> (var_nor ());
     }
-    
-    save_cloud ("noized_cloud.pcd", *noized_cloud);
 }
 
 double
@@ -151,41 +149,55 @@ compute_metric_mean(pcl::PointCloud<pcl::PointXYZ> &cloud)
 }
 
 int
-main()
+main(int argc, char **argv)
 {
     int w = 10;
     int h = 1;
     float mean = 0; //матожидание
     float disp = 0.1; //дисперсия
-    //create_cloud(w, h); //создаем облако точек с необходимым количесвтом точек и структурой
 
+    //create_cloud(w, h); //создаем облако точек с необходимым количесвтом точек и структурой
+                          //этот метод создавался для того, чтобы тестить программу на малых количествах точек, для удобства
+
+    if (argc != 3)
+    {
+        std::cerr << "must be one input PCD file and one output PCD file to continue" << std::endl;
+        return -1;
+    }
+    
     //загружаем в cloud наше исходное облако
+
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    if (!load_cloud ("test_pcd.pcd", *cloud))
+    if (!load_cloud (argv[1], *cloud))
         return (-1);
 
     for (size_t i = 0; i < cloud->points.size (); ++i)
-    std::cout << "    " << cloud->points[i].x
-              << " "    << cloud->points[i].y
-              << " "    << cloud->points[i].z << std::endl;
+        std::cout << "    " << cloud->points[i].x
+                  << " "    << cloud->points[i].y
+                  << " "    << cloud->points[i].z << std::endl;
 
-    //добавляем к нему шум 
-    add_gaussian_noise(*cloud, mean, disp);
-    //загружаем в clous_mod новое зашумленное облако
-
+    //добавляем к облаку шум 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_mod (new pcl::PointCloud<pcl::PointXYZ>);
-    if (!load_cloud ("noized_cloud.pcd", *cloud_mod))
+    add_gaussian_noise(*cloud, *cloud_mod, mean, disp);
+    save_cloud (argv[2], *cloud_mod);
+    /*
+    //загружаем в noized_cloud новое зашумленное облако
+    //вообще не очень понятно зачем сохранять и снова загружать модифицированное облако, 
+    //когда удобно работать сразу внутри программы
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr noized_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    if (!load_cloud ("test_pcd_noized.pcd", *noized_cloud))
         return (-1);
 
-    for (size_t i = 0; i < cloud_mod->points.size (); ++i)
-    std::cout << "    " << cloud_mod->points[i].x
-              << " "    << cloud_mod->points[i].y
-              << " "    << cloud_mod->points[i].z << std::endl;
-    
+    for (size_t i = 0; i < noized_cloud->points.size (); ++i)
+        std::cout << "    " << noized_cloud->points[i].x
+                  << " "    << noized_cloud->points[i].y
+                  << " "    << noized_cloud->points[i].z << std::endl;
+    */
     
     //считаем и выводим метрику
-    double abs_m;
-    double mean_m;
+    double abs_m; //абсолютная разница 
+    double mean_m; //средняя разница 
     abs_m = abs(compute_metric_abs(*cloud_mod) - compute_metric_abs(*cloud));
     mean_m = abs(compute_metric_mean(*cloud_mod) - compute_metric_mean(*cloud));
     std::cout << "абсолютная разница: " << abs_m 
